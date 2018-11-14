3078aa878b630f7dfba597bb611831b2fb9eddb3
Bob Leers <bob@novacode.nl>
2018-11-14 10:09:37 +0100

    celery_example: Queue create Line(s) with countdown.

--- celery_example/models/celery_example.py
+++ celery_example/models/celery_example.py
@@ -13,14 +13,49 @@ class CeleryExample(models.Model):
     _name = 'celery.example'
     _description = 'Celery Example'
 
-    name = fields.Char(default='Celery Example')
+    name = fields.Char(default='Celery Example', required=True)
+    lines = fields.One2many('celery.example.line', 'example_id', string='Lines')
 
     @api.multi
-    def action_schedule_log_message(self):
-        self.env["celery.task"].call_task("celery.example", "schedule_log_message")
+    def action_queue_example_line(self):
+        self.env["celery.task"].call_task("celery.example", "queue_example_line", example_id=self.id)
+
+    @api.multi
+    def action_queue_countdown_example_line(self):
+        # Several Celery params can be set by the celery kwarg.
+        celery = {'countdown': 10}
+        self.env["celery.task"].call_task("celery.example", "queue_countdown_example_line", example_id=self.id, celery=celery)
 
     @api.model
-    def schedule_log_message(self, task_uuid, **kwargs):
-        msg = 'CELERY called task: model [%s] and method [schedule_log_message].' % self._name
+    def queue_example_line(self, task_uuid, **kwargs):
+        example_id = kwargs.get('example_id')
+        self.env['celery.example.line'].create({
+            'name': 'Created by queued task',
+            'example_id': example_id
+        })
+        msg = 'CELERY called task: model [%s] and method [queue_example_line].' % self._name
         _logger.info(msg)
         return msg
+
+    @api.model
+    def queue_countdown_example_line(self, task_uuid, **kwargs):
+        example_id = kwargs.get('example_id')
+        self.env['celery.example.line'].create({
+            'name': 'Created by queued task with countdown (10 sec)',
+            'example_id': example_id
+        })
+        msg = 'CELERY called task: model [%s] and method [queue_countdown_example_line].' % self._name
+        _logger.info(msg)
+        return msg
+
+    @api.multi
+    def refresh_view(self):
+        return True
+
+
+class CeleryExampleLine(models.Model):
+    _name = 'celery.example.line'
+    _description = 'Celery Example Line'
+
+    name = fields.Char(required=True)
+    example_id = fields.Many2one('celery.example', string='Example', required=True, ondelete='cascade')
