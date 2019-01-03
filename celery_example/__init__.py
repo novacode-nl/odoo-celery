1873567aa541db07088262494b741ac848255d1e
Bob Leers <bob@novacode.nl>
2019-01-04 00:48:03 +0100

    celery: Model access. Remove utf-8 coding statement from Python files.

--- celery_example/__init__.py
+++ celery_example/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Nova Code (http://www.novacode.nl)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
--- celery_example/__manifest__.py
+++ celery_example/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Nova Code (http://www.novacode.nl)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 {
@@ -13,6 +12,7 @@
     'depends': ['celery'],
     'data': [
         'data/celery_example_data.xml',
+        'security/ir_model_access.xml',
         'views/celery_example_views.xml'
     ],
     'images': [
