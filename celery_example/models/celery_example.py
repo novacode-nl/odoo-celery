3d864c4a09e1d35923f32cc146c82548b43b9ede
Bob Leers <bob@novacode.nl>
2019-12-16 10:33:29 +0100

    [ADD] Task after_commit option: create and call task (MQ) after commit of transaction.

--- celery_example/models/celery_example.py
+++ celery_example/models/celery_example.py
@@ -29,6 +29,17 @@ class CeleryExample(models.Model):
         self.env["celery.task"].call_task("celery.example", "task_with_reference", example_id=self.id, celery_task_vals=celery_task_vals, celery=celery)
 
     @api.multi
+    def action_task_after_commit(self):
+        celery = {
+            'countdown': 10, 'retry': True,
+            'retry_policy': {'max_retries': 2, 'interval_start': 2}
+        }
+        celery_task_vals = {
+            'ref': 'celery.example.task_after_commit'
+        }
+        self.env["celery.task"].call_task("celery.example", "task_after_commit", example_id=self.id, celery_task_vals=celery_task_vals, celery=celery, after_commit=True)
+
+    @api.multi
     def action_task_with_error(self):
         celery = {
             'countdown': 2,
@@ -70,6 +81,18 @@ class CeleryExample(models.Model):
     @api.model
     def task_with_reference(self, task_uuid, **kwargs):
         task = 'task_with_reference'
+        example_id = kwargs.get('example_id')
+        self.env['celery.example.line'].create({
+            'name': task,
+            'example_id': example_id
+        })
+        msg = 'CELERY called task: model [%s] and method [%s].' % (self._name, task)
+        _logger.info(msg)
+        return msg
+
+    @api.model
+    def task_after_commit(self, task_uuid, **kwargs):
+        task = 'task_after_commit'
         example_id = kwargs.get('example_id')
         self.env['celery.example.line'].create({
             'name': task,
