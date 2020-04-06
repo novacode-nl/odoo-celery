412d3458a9461d8840a7797ba5e127e7ef7b7840
Bob Leers <bob@novacode.nl>
2020-04-06 19:09:28 +0200

    [FIX] call_task() => Access Denied by ACLs for operation: read, model: celery.task.setting.
    In call_task() the search of celery.task.setting results in "Access Denied by ACLs for operation: read, model: celery.task.setting".
    Solved by calling the celery.task.setting search() with sudo().

--- celery/__manifest__.py
+++ celery/__manifest__.py
@@ -4,7 +4,7 @@
     'name': 'Celery',
     'summary': 'Celery (Distributed Task Queue)',
     'category': 'Extra Tools',
-    'version': '0.19',
+    'version': '0.20',
     'description': """Execute Odoo methods by Celery worker tasks.""",
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
